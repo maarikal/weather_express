fae559ef49ac302c7b1be955b5edd3326f33ed6d
Maarika Lukk <maarika.lukk@khk.ee>
2022-02-21 15:38:24 +0200

    Added API data to project template file

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
-const express = require('express');
-const app = express();
-/*
+const express = require('express')
+const app = express()
 const path = require('path')
 const fetch = require('node-fetch')
 
@@ -8,36 +7,24 @@ app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 
 const key = 'd43b9bc94518afe99cb9ba423d828c5b';
-let city = 'Tartu'
-*/
+let city = 'Tartu';
 
-app.get('/user/:username', (req, res) => {
-    // get parameter data from address row
-    let user = req.params.username;
-    // use this data in template
-    res.render('index.ejs', {username: user});
-});
-
-app.get('/demo', (req, res) => {
-    res.render('index.ejs');
-});
-
-app.listen(3000, ()=> {
-    console.log('Server started on http://localhost:3000');
-});
-
-/*
 app.get('/', function (req, res) {
-    fetch(`https://api.openweathermap.org/data/2.5/weatger?q=${
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${
         city}&appid=${key}`)
         .then((response) => {
         return response.json()
     })
         .then((data) => {
-        console.log(data);
+            let description = data.weather[0].description
+            let city = data.name
+            let temp = Math.round(parseFloat(data.main.temp)-273.15)
+            res.render('index', {
+                description: description,
+                city: city,
+                temp: temp
+            })
     })
-    res.render('index')
 })
 
 app.listen(3000)
-*/
